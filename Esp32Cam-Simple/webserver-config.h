5af5587009387e9713f4c1adaa8fe56e12fda305
PB <petr.brouzda@gmail.com>
2026-02-11 09:06:20 +0100

    oprava IP adresy aby captive portál fungoval na Samsungu

--- Esp32Cam-Simple/webserver-config.h
+++ Esp32Cam-Simple/webserver-config.h
@@ -6,9 +6,15 @@
 #define AP_SSID "EspCam_"
 #define AP_PASSWORD "password"
 
-// Pokud váš mobil nedělá automaticky přesměrování, zadejte do prohlížeče http://192.168.59.1
-IPAddress local_ip(192,168,59,1);
-IPAddress gateway(192,168,59,1);
+// Pokud váš mobil nedělá automaticky přesměrování, zadejte do prohlížeče http://200.200.200.1
+
+// Řešení captive portálu pro Samsungy:
+// https://github.com/esp8266/Arduino/commit/d5265aa5ac4995b65bce3e29025307cfd7ae0d0f#diff-9978b19703e34e9504dcdda781907935
+// Took me quite a while to figure this out, but according to this issue (espressif/arduino-esp32#1037), in order to get 
+// a captive notification to show or the popup to open, the DNS server must resolve to a public IP. 
+// It will not work with a private one (e.g. 192.168.4.1).
+IPAddress local_ip(200,200,200,1);
+IPAddress gateway(200,200,200,1);
 IPAddress subnet(255,255,255,0);
 
 #define WEB_PORT 80 
